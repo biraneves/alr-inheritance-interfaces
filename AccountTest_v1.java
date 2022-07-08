6306265b69544a01c85c6c9826499b3d46036342
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-08 19:41:33 -0300

    Simple test for classes Account and Customer.

---
+++ AccountTest_v1.java
@@ -0,0 +1,15 @@
+class AccountTest {
+
+    public static void main(String[] args) {
+        
+        Account account = new Account(1337, 23334);
+        account.deposit(200.0);
+        System.out.println(account.getBalance());
+
+        account.setAgency(570);
+        System.out.println(account.getAgency());
+        System.out.println("The total number of accounts is " + Account.getTotal());
+
+    }
+
+}

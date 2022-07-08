9fa6c1687f2c832247ca0e631deb9dd8d59cbf83
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-08 19:37:16 -0300

    Initial class Customer.

---
+++ Customer.java
@@ -0,0 +1,43 @@
+public class Customer {
+
+    private String name;
+    private String id;
+    private String profession;
+
+    public String getName() {
+
+        return this.name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getId() {
+
+        return this.id;
+
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+
+    }
+
+    public String getProfession() {
+
+        return this.profession;
+
+    }
+
+    public void setProfession(String profession) {
+
+        this.profession = profession;
+
+    }
+
+}

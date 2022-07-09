641c19720460e2d4f679615413cdd3d6c64f6140
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-09 17:55:26 -0300

    Class Designer

---
+++ Designer.java
@@ -0,0 +1,15 @@
+public class Designer extends Employee {
+
+    public Designer(String name, String id, double salary) {
+
+        super(name, id, salary);
+
+    }
+
+    public double getBonus() {
+
+        return 150;
+
+    }
+
+}

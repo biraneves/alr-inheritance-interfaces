9d7ca6ec21050d985eaee3f2fae7522dc79ab2af
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-09 08:18:48 -0300

    Class VideoEditor with a constructor.

--- VideoEditor_v1.java
+++ VideoEditor_v1.java
@@ -1,5 +1,11 @@
 class VideoEditor extends Employee {
 
+    public VideoEditor(String name, String id, double salary) {
+
+        super(name, id, salary);
+
+    }
+
     public double getBonus() {
 
         return 100;
